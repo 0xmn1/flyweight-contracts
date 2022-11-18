00a1f8a74726184db0e36029d68fff6567ea7e63
m n <116815127+0xmn1@users.noreply.github.com>
2022-11-18 14:33:32 +1100

    Move TokenWhitelist contract from "libraries" folder to "contracts" folder

--- src/contracts/Flyweight.sol
+++ src/contracts/Flyweight.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../libraries/TokenWhitelist.sol";
+import "./TokenWhitelist.sol";
 
 contract Flyweight {
     address constant public UNISWAP_ROUTER_ADDRESS = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
